4cbd1f0a35bec38e0bf2afb0fa16d1db73e8cfbd
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-12-12 05:20:25 +0500

    feat: make view of calculator an styled it

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
 import '../scss/style.scss';
 import '../index.html';
 
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if(number === '.' && this.currentOperand.includes('.')) {
+      return;
+    }
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+}
+
+
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
